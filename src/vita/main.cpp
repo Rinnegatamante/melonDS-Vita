0f40a78fe62bf0c120dc7e2ddeebafa3afc4980c
Rinnegatamante <rinnegatamante@gmail.com>
2018-12-05 20:13:19 +0100

    Not spamming key presses with touchscreen.

--- src/vita/main.cpp
+++ src/vita/main.cpp
@@ -413,6 +413,7 @@ int melon_main(unsigned int argc, void *argv)
     Framebuffer = (u32*)vita2d_texture_get_datap(vram_buffer);
 
     uint32_t keys[] = { SCE_CTRL_CROSS, SCE_CTRL_CIRCLE, SCE_CTRL_SELECT, SCE_CTRL_START, SCE_CTRL_RIGHT, SCE_CTRL_LEFT, SCE_CTRL_UP, SCE_CTRL_DOWN, SCE_CTRL_RTRIGGER, SCE_CTRL_LTRIGGER, SCE_CTRL_SQUARE, SCE_CTRL_TRIANGLE };
+    bool Touching = false;
     
     while (true)
     {
@@ -464,14 +465,21 @@ int melon_main(unsigned int argc, void *argv)
                 default:
                     break;
                 }
-                NDS::PressKey(16 + 6);
+                if (!Touching) NDS::PressKey(16 + 6);
                 NDS::TouchScreen(x, y);
+                Touching = true;
+            }else if (Touching)
+            {
+                NDS::ReleaseKey(16 + 6);
+                NDS::ReleaseScreen();
+                Touching = false;
             }
         }
-        else
+        else if (Touching)
         {
             NDS::ReleaseKey(16 + 6);
             NDS::ReleaseScreen();
+            Touching = false;
         }
         
         vita2d_start_drawing();
